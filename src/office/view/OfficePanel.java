9b2b5087e5a4ce0fe35e57d9d1b789f60f108520
Jordan-Ray <jordan.b.ray40@gmail.com>
2014-05-22 08:05:38 -0600

    Panel, Layout, and Listeners

--- src/office/view/OfficePanel.java
+++ src/office/view/OfficePanel.java
@@ -2,5 +2,27 @@ package office.view;
 
 public class OfficePanel
 {
+	 OfficePanel(OfficeController baseController)
+		{
+			this.baseController = baseController;
 
+			setupPanel();
+			setupLayout();
+			setupListeners();
+		}
+	 
+	 setupPanel()
+	 {
+		 
+	 }
+	 
+	 setupLayout()
+	 {
+		 
+	 }
+	 
+	 setupListeners()
+	 {
+		 
+	 }
 }

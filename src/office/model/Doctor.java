a7809e40dc5703e620841c2309461ed69bd1e461
Jordan-Ray <jordan.b.ray40@gmail.com>
2014-05-22 08:20:02 -0600

    Doctor and Patient getters and setters.

--- src/office/model/Doctor.java
+++ src/office/model/Doctor.java
@@ -2,5 +2,38 @@ package office.model;
 
 public class Doctor
 {
+	private String name;
+	private String specality;
+	
+	public Doctor()
+	{
+	
+	}
+	
+	public Doctor(String name, String specality)
+	{
+		this.name = name;
+		this.specality = specality;
+	}
 
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getSpecality()
+	{
+		return specality;
+	}
+
+	public void setSpecality(String specality)
+	{
+		this.specality = specality;
+	}
+	
 }

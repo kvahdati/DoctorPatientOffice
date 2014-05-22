fbcd1b1db917cfd6028a2f122d213a33c64a38e9
Jordan-Ray <jordan.b.ray40@gmail.com>
2014-05-22 08:42:14 -0600

    Patient

--- src/office/model/Patient.java
+++ src/office/model/Patient.java
@@ -1,15 +1,69 @@
 package office.model;
 
+import java.util.ArrayList;
+
 public class Patient
 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
+	private String name;
+	private int age;
+	private String gender;
+	private ArrayList<String> symptoms;
+	
+	public Patient()
 	{
-		// TODO Auto-generated method stub
+		name = "Bob";
+		age = 20;
+		gender = "confused";
+		symptoms = new ArrayList<String>();
+	}
 
+	public Patient(String name, int age, String gender, ArrayList<String> symptoms)
+	{
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		symptoms = new ArrayList<String>();
+		
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public String getGender()
+	{
+		return gender;
+	}
+
+	public void setGender(String gender)
+	{
+		this.gender = gender;
+	}
+
+	public ArrayList<String> getSymptoms()
+	{
+		return symptoms;
+	}
+
+	public void setSymptoms(ArrayList<String> symptoms)
+	{
+		this.symptoms = symptoms;
 	}
 
 }

5c8957f3fd1c204b249a77be6d00a5ec99997be9
belsthomaz <isabellazamoht@gmail.com>
2014-05-22 08:12:45 -0600

    controller part 1
    declared two variables, getting start to work

--- src/office/control/OfficeController.java
+++ src/office/control/OfficeController.java
@@ -1,14 +1,42 @@
 package office.control;
 
+import office.view.OfficeFrame;
+
 public class OfficeController
 {
+	private OfficeFrame appFrame;
 
+	private int patientCount;
 	
+	public OfficeController()
+	{
+		appFrame = new OfficeFrame();
+		patientCount = 0;
+	}
+	
+	public OfficeFrame getAppFrame()
+	{
+		return appFrame;
+	}
+
+	public void setAppFrame(OfficeFrame appFrame)
+	{
+		this.appFrame = appFrame;
+	}
+
+	public int getPatientCount()
+	{
+		return patientCount;
+	}
+
+	public void setPatientCount(int patientCount)
+	{
+		this.patientCount = patientCount;
+	}
 
 	public void start()
 	{
-		
-		
+		appFrame = new OfficeFrame(this);
 	}
 
 }

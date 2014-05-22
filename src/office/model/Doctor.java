b450493f3eed33c76d39499225855d984e2db8da
Jordan-Ray <jordan.b.ray40@gmail.com>
2014-05-22 08:55:24 -0600

    Hurt

--- src/office/model/Doctor.java
+++ src/office/model/Doctor.java
@@ -4,23 +4,41 @@ public class Doctor
 {
 	private String name;
 	private String specality;
+	private String patientList;
 	
-	public Doctor()
+	
+	public void docBrown()
 	{
 		name = "Doc Brown";
 		specality = "Time Travel";
-		
+	}
+	
+	public void docHurt()
+	{
+		name = "Doc Hurt";
+		specality = "Space Travel";
 	}
 	
 	public Doctor(String name, String specality)
 	{
 		this.name = name;
 		this.specality = specality;
+		this.patientList = patientList;
 	}
 
 	public String getName()
 	{
 		return name;
+	}
+
+	public String getPatientList()
+	{
+		return patientList;
+	}
+
+	public void setPatientList(String patientList)
+	{
+		this.patientList = patientList;
 	}
 
 	public void setName(String name)

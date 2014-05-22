58e7d0bb3060d0156198bf47fcbc4726ddd88557
kvahdati <kvahdati@gmail.com>
2014-05-22 08:41:40 -0600

    initialized some panel parts

--- src/office/view/OfficePanel.java
+++ src/office/view/OfficePanel.java
@@ -2,6 +2,7 @@ package office.view;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -9,15 +10,16 @@ import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 import office.control.OfficeController;
+import office.model.Patient;
 
 public class OfficePanel extends JPanel
 {
 	private OfficeController myController;
 	private SpringLayout myLayout;
 	private JCheckBox docdoc;
-	private JCheckBox patientz;
+	private JComboBox patientz;
 	private JTextField patientID;
-	private JTextArea symptomsLists;
+	private JTextArea symptomsList;
 	private JLabel title;
 	private JLabel patientzLabel;
 	private JLabel doctorLabel;
@@ -34,9 +36,17 @@ public class OfficePanel extends JPanel
 		{
 			this.myController = myController;
 			myLayout = new SpringLayout();
-			editButton = new JButton("edit");
-			addButton = new JButton("")
-			
+			editButton = new JButton("EDIT");
+			addButton = new JButton("ADD");
+			displayButton = new JButton("DISPLAY");
+			title = new JLabel("Doctors of Space and Time");
+			patientzLabel = new JLabel("Patients");
+			doctorLabel = new JLabel("Doctors");
+			symptomsLabel = new JLabel("Symptoms");
+			docdoc = new JCheckBox();
+			patientz = new JComboBox<Patient>();
+			patientID = new JTextField(15);
+			symptomsList = new JTextArea(1,2);
 			setupPanel();
 			setupLayout();
 			setupListeners();
